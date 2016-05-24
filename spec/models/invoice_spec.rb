291d435edae1e5b984bc4f1337ec486ba6b6e414
Michal Juriš <michaljuris@gmail.com>
2016-05-24 12:05:58 +0200

    Add tests

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Invoice, type: :model do
+describe Invoice do
+  describe '#price_with_vat' do
+    it 'returns price including VAT and rounds it correctly' do
+      invoice = Invoice.new(price: 33, vat_rate: 33)
 
+      expect(invoice.price_with_vat).to eq 43.89
+    end
+  end
 end
